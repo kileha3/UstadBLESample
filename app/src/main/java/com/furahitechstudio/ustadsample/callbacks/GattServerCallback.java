c892d4ce2ac417d59a3a6a2ae3418fea4695a9ce
Lukundo Kileha <lkileha@gmail.com>
2018-07-25 20:01:15 +0400

    Modified the class to make sure it handle packets on common method provided

--- app/src/main/java/com/furahitechstudio/ustadsample/callbacks/GattServerCallback.java
+++ app/src/main/java/com/furahitechstudio/ustadsample/callbacks/GattServerCallback.java
@@ -7,12 +7,14 @@ import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattServerCallback;
 import android.bluetooth.BluetoothProfile;
 import com.furahitechstudio.ustadsample.manager.BluetoothManagerShared;
+import com.furahitechstudio.ustadsample.models.NetworkNode;
 import com.furahitechstudio.ustadsample.utils.BleAndroidUtils;
 import com.furahitechstudio.ustadsample.utils.LogWrapper;
 import java.io.ByteArrayOutputStream;
 
 import static com.furahitechstudio.ustadsample.manager.BluetoothManagerShared.CLIENT_CONFIGURATION_DESCRIPTOR_UUID;
 import static com.furahitechstudio.ustadsample.manager.BluetoothManagerShared.ENTRY_STATUS_REQUEST;
+import static com.furahitechstudio.ustadsample.manager.BluetoothManagerShared.ENTRY_STATUS_RESPONSE;
 import static com.furahitechstudio.ustadsample.manager.BluetoothManagerShared.SERVICE_UUID;
 
 public class GattServerCallback extends BluetoothGattServerCallback {
@@ -20,11 +22,7 @@ public class GattServerCallback extends BluetoothGattServerCallback {
 
   private BluetoothManagerShared bluetoothManager;
 
-  private boolean isWaitingForExtraPackets = false;
 
-  private int contentLength = 0;
-
-  private ByteArrayOutputStream outputStream = null;
 
   public GattServerCallback(BluetoothManagerShared bluetoothManager) {
     this.bluetoothManager = bluetoothManager;
@@ -62,41 +60,11 @@ public class GattServerCallback extends BluetoothGattServerCallback {
     super.onCharacteristicWriteRequest(device, requestId, characteristic, preparedWrite,
         responseNeeded, offset, value);
 
-    LogWrapper.log(false, "onCharacteristicWriteRequest for "+characteristic.getUuid().toString());
     if (SERVICE_UUID.equals(characteristic.getUuid())) {
+      LogWrapper.log(false, "onCharacteristicWriteRequest for "+characteristic.getUuid().toString());
       bluetoothManager.acknowledgeRequest(BleAndroidUtils.getNetworkNode(device), requestId, BluetoothGatt.GATT_SUCCESS, 0, null);
-      processPackets(value);
+      bluetoothManager.processPackets(BleAndroidUtils.getNetworkNode(device),value);
     }
-  }
-
-
-  private void processPackets(byte [] value){
-    byte clientRequest = BleAndroidUtils.getRequestType(value);
-    byte [] actualPayLoad = null;
-    if(ENTRY_STATUS_REQUEST == clientRequest){
-      LogWrapper.log(false, "Requesting course status");
-      outputStream = new ByteArrayOutputStream();
-      isWaitingForExtraPackets = true;
-      contentLength = BleAndroidUtils.getContentLength(value);
-    }
-
-
-    if(isWaitingForExtraPackets && outputStream!=null){
-      outputStream.write(value, 0, value.length);
-      actualPayLoad = BleAndroidUtils.getActualPayLoad(outputStream.toByteArray());
-    }
-
-    if(outputStream !=null){
-      LogWrapper.log(false, "Waiting "+ isWaitingForExtraPackets +" Request "+clientRequest
-          +" Length "+contentLength+" Payload: "+actualPayLoad.length);
-
-      if(contentLength == actualPayLoad.length){
-        LogWrapper.log(false,"Transfer completed\n"
-            +BleAndroidUtils.decompress(actualPayLoad));
-      }
-    }
-
-
   }
 
 
